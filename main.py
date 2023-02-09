a381186135f2e6d1b25a236de388be6c537bb6bf
Mykola Bezrukyi <mykola_bezrukyi@epam.com>
2023-02-09 02:27:23 +0200

    rename collections package

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # import our own 'python_basics' package
 # import our own 'collections' package
-import collections
+import collections_python
 import python_basics
 
 
@@ -19,11 +19,11 @@ def execute_python_basics_home_task() -> None:
 
 def execute_collections_home_task() -> None:
     # generate a list of random dicts due to 'generate_list_dicts' func and assign it to 'list_dicts" var
-    list_dicts = collections.generate_list_dicts()
+    list_dicts = collections_python.generate_list_dicts()
     # print 'list_dicts' var to a console
     print(list_dicts)
     # create a common dict with 'create_common_dict' func and assign the result to 'common_dict' var
-    common_dict = collections.create_common_dict(list_dicts)
+    common_dict = collections_python.create_common_dict(list_dicts)
     # print 'common_dict' var to a console
     print(common_dict)
 
