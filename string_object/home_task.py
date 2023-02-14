f9a3229b2fd54a61cb3ab1b8f01e141052ad00ee
Mykola Bezrukyi <mykola_bezrukyi@epam.com>
2023-02-14 23:11:41 +0200

    fix string home task

--- string_object/home_task.py
+++ string_object/home_task.py
@@ -1,24 +1,26 @@
+import re
+
 PARAGRAPH_NUMBER = 3
 
 
 def normalize_string(s: str) -> str:
-    l = s.split('\n')
-    for i, sentence in enumerate(l):
-        sentence_l = sentence.split('. ')
-        l[i] = '. '.join(list(map(str.capitalize, sentence_l)))
-    return '\n'.join(l)
+    lst = s.split('\n')
+    for i, sentence in enumerate(lst):
+        sentence_lst = sentence.split('. ')
+        lst[i] = '. '.join(list(map(str.capitalize, sentence_lst)))
+    return '\n'.join(lst)
 
 
 def add_special_sentence(s: str, special_sentence: str) -> str:
-    l = s.split('\n')
+    lst = s.split('\n')
     whitespaces_count = 0
-    for i, sentence in enumerate(l):
+    for i, sentence in enumerate(lst):
         if sentence in ('', ' '):
             whitespaces_count += 1
         if is_paragraph_number(i, whitespaces_count):
-            l[i] = l[i] + ' ' + special_sentence
+            lst[i] = lst[i] + ' ' + special_sentence
             break
-    return '\n'.join(l)
+    return '\n'.join(lst)
 
 
 def is_paragraph_number(i: int, whitespaces_count: int) -> bool:
@@ -26,12 +28,7 @@ def is_paragraph_number(i: int, whitespaces_count: int) -> bool:
 
 
 def get_special_sentence(s: str) -> str:
-    return (' '.join(
-        [
-            sentence.split(' ')[-1]
-            for sentence in s.split('.')
-        ]
-    ).strip() + '.').capitalize()
+    return ' '.join(re.findall(r'\s(\w+)\.', s)).capitalize() + '.'
 
 
 def replace_incorrect_value(s: str) -> str:
